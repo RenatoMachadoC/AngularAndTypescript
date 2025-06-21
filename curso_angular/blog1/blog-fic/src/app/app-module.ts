210bd4a75fd69a04e3df1240552f4d77c703d6b9
Renato Machado <machado.renato113@gmail.com>
2025-06-21 17:33:05 -0300

    Atualização do projeto blog1

--- curso_angular/blog1/blog-fic/src/app/app-module.ts
+++ curso_angular/blog1/blog-fic/src/app/app-module.ts
@@ -1,13 +1,15 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
+import { AppRoutingModule } from './app-routing.module';
 import { App } from './app';
 import { MenuBar } from './components/menu-bar/menu-bar';
 import { MunuTitle } from './components/munu-title/munu-title';
 import { BigCard } from './components/big-card/big-card';
 import { SmallCard } from './components/small-card/small-card';
 import { Home } from './pages/home/home';
+import { Content } from './pages/content/content';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { Home } from './pages/home/home';
     MunuTitle,
     BigCard,
     SmallCard,
-    Home
+    Home,
+    Content
   ],
   imports: [
     BrowserModule,
